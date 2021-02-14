af6cc8e5906ad8a33926fd3677a0edeaacdd752a
yoo.chrisx15@gmail.com <yoo.chrisx15@gmail.com>
2021-02-13 20:42:35 -0700

    newest edit with GUI edits

--- code&files/MLData1.py
+++ code&files/MLData1.py
@@ -1,6 +1,28 @@
 import pandas as pd
 from sklearn import linear_model
 
+import PySimpleGUI as sg
+
+sg.theme("DarkBlue12")
+
+sqft = sg.InputText()
+beds = sg.InputText()
+baths = sg.InputText()
+
+layout = [ [sg.Text("Enter square footage of the house:"), sqft], 
+[sg.Text("Enter the number of bedrooms:"), beds], 
+[sg.Text("Enter number of bathrooms"), baths] ]
+
+windowScreen = sg.Window("Am-KoH Painting Cost Estimator", layout)
+
+while True:
+    event, values = windowScreen.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+windowScreen.close()   
+
+"""
 df = pd.read_csv("Machine Learning/Project1/BusinessData1.csv")
 
 X = df[['SqFt','Bed','Bath']]
@@ -12,3 +34,4 @@ regression.fit(X, y)
 
 estimateHousePredict = regression.predict([[2954,4,3]])
 print(estimateHousePredict)
+"""
